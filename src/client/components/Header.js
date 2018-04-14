5fc9597855555ddebc1608529f5aea3e211674aa
Maksim Golitsyn <maxim@infofree.ru>
2018-04-15 01:25:06 +0500

    Header Customization

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -4,16 +4,26 @@ import { connect } from 'react-redux';
 
 
 const Header = ({ auth }) => {
-  console.log('My auth status is ', auth);
- return (
-   <div>
-     <Link to="/">React SSR</Link>
-   </div>
- );
+  console.log('My auth status is ', auth); // todo
+
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
+
+  return (
+    <div>
+      <Link to="/">React SSR</Link>
+      <div>
+        <Link to="/users">Users</Link>
+        <Link to="/admins">Admins</Link>
+        {authButton}
+      </div>
+    </div>
+  );
 };
 
-const mapStateToProps = ({ auth }) => {
-  return { auth };
-};
+const mapStateToProps = ({ auth }) => ({ auth });
 
 export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -9,21 +9,22 @@ import createStore from './helpers/createStore';
 
 const app = express();
 
-app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
-  proxyReqOptDecorator(opts) {
-    opts.headers['x-forwarded-host'] = 'localhost:3000';
-    return opts;
-  },
-}));
+app.use(
+  '/api',
+  proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+      opts.headers['x-forwarded-host'] = 'localhost:3000';
+      return opts;
+    },
+  }),
+);
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
   const store = createStore(req);
 
 
-  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
-    return route.loadData ? route.loadData(store) : null;
-  });
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => (route.loadData ? route.loadData(store) : null));
 
   Promise.all(promises)
     .then(() => {
