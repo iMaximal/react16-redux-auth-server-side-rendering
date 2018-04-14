c5864e093c3dead08da448a209bf4b0d55b1931f
Maksim Golitsyn <maxim@infofree.ru>
2018-04-14 23:31:25 +0500

    Server Axios Instance

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ import createStore from './helpers/createStore';
 
 const app = express();
 
-app.use('/api', proxy('http://react-ssr-api.heroruapp.com', {
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
   proxyReqOptDecorator(opts) {
-    opts.header['x-forwarded-host'] = 'localhost:3000';
+    opts.headers['x-forwarded-host'] = 'localhost:3000';
     return opts;
   },
 }));
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  const store = createStore();
+  const store = createStore(req);
 
 
   const promises = matchRoutes(Routes, req.path).map(({ route }) => {
