67d1629d18c6c2c215c06c476170ae72c8bbf536
Maksim Golitsyn <maxim@infofree.ru>
2018-04-15 21:56:26 +0500

    Admins Action Creator and Reducer

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import usersReducers from './usersReducer';
+import adminsReducer from './adminsReducer';
 
 export default combineReducers({
+  admins: adminsReducer,
   auth: authReducer,
   users: usersReducers,
 });
